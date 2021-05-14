9204c290e0e9be39264904ce2acce3f4cc41be0d
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-14 12:54:08 -0700

    add good fail to GetVendorInfo *Red Test

--- PierresBakery.Tests/ModelTests/VendorTests.cs
+++ PierresBakery.Tests/ModelTests/VendorTests.cs
@@ -11,12 +11,24 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendorConstructor_Constructor()
     {
-      
       Vendor newVendor = new Vendor("TEST");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-      
+    }
 
+    [TestMethod]
+    public void GetVendorInformation_ReturnsDescription_String()
+    {
+      //Arrange
+      string VendorName = "marge";
+      string VendorDescription = "wigDealer";
+      Vendor newVendor = new Vendor(VendorName, VendorDescription);
       
+      //Act
+      string result = newVendor.VendorName;
+
+      //Assert
+      Assert.AreEqual(VendorName, result);
+
     }
   }
 }
